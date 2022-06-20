ddb6f2f832a03830f280a6afd1aae8bf01f01ab9
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-06-20 22:26:57 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.45 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        int Max = 0;
+        int Min = 0;
+        
+        //우선순위큐를 이용하여 queue 적용
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        StringTokenizer token = null;
+        for(int i = 0; i < operations.length; i++){
+            
+            token = new StringTokenizer(operations[i]);
+            String ID = token.nextToken();
+            int num = Integer.parseInt(token.nextToken());
+            
+            if(ID.equals("I")){
+                que.add(num);
+            }else if(ID.equals("D") && !que.isEmpty()){
+                if(num == -1)
+                    que.poll();
+                
+                else{
+                    PriorityQueue<Integer> newQue = new PriorityQueue<>();
+                    int size = que.size()-1;
+                    for(int j = 0; j < size; j++){
+                        newQue.add(que.poll());
+                    }
+                    
+                    que.clear();
+                    for(int no : newQue){
+                        que.add(no);
+                    }
+                }
+                    
+            }
+        }
+        
+        if(que.size() > 1){
+            
+            Min = que.poll();
+            int size = que.size()-1;
+            for(int i = 0; i < size; i++){
+                que.poll();
+            }
+            Max = que.poll();
+        }else if(que.size() == 1){
+            Min = que.poll();
+            Max = Min;
+            
+        }
+        answer[0] = Max;
+        answer[1] = Min;
+        
+        return answer;
+    }
+}
