b8fedb475d581daa47b2a809a2466beb0ca7cef1
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-08-18 23:44:31 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 9.25 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,19 +1,14 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        //싹다 소문자처리를 하고
-        //공백을 기준으로 첫 글자가 알파벳이라면 그 글자만 대문자로 바꿔주기
+        //공백을 기준으로 첫 글자만 대문자로 바꿔주기
+        //나머지는 다 소문자 처리
         for(int i = 0; i < s.length(); i++){
             if(i == 0 || (s.charAt(i-1) == ' ' && s.charAt(i) != ' ')){
-                if(s.charAt(i)-'0' >= 17 && s.charAt(i)-'0' <= 74){
                     answer += Character.toUpperCase(s.charAt(i));
-                }else
-                    answer += Character.toLowerCase(s.charAt(i));
             }else
                 answer += Character.toLowerCase(s.charAt(i));
         }
-        
-        
         
         return answer;
     }
