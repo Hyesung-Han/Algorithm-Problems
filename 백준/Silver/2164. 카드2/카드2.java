db7dcc98a1286942ff3937e4d303076a488b6f54
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-09-07 12:34:44 +0900

    [Silver IV] Title: 카드2, Time: 136 ms, Memory: 43136 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	static int N;
+	
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		//linkedlist에 카드 번호를 순서대로 넣은 후 que를 통해 나가고 들어가기를 반복할 예정
+		
+		Queue<Integer> que = new LinkedList<Integer>();
+		
+		for (int i = 1; i <= N; i++) {
+			que.offer(i);			
+		}
+		
+		while(que.size() != 1) {
+			que.poll();
+			que.offer(que.poll());
+		}
+		System.out.println(que.poll());
+	}
+
+}
