3ce594d46d977966f100f54f6cc5be10178f1c7d
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-11-17 13:05:59 +0900

    [Gold IV] Title: 겹치는 선분, Time: 1744 ms, Memory: 357200 KB -BaekjoonHub

--- 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
@@ -15,8 +15,7 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		StringTokenizer token;
 		line = new ArrayList<>();
-//		int Max = Integer.MIN_VALUE;
-//		int Min = Integer.MAX_VALUE;
+
 		for(int i = 0; i < N; i++) {
 			token = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(token.nextToken());
@@ -24,18 +23,13 @@ public class Main {
 			
 			line.add(new LineNum(s,1));
 			line.add(new LineNum(e,-1));
-//			Min = Math.min(Min, s);
-//			Max = Math.max(Max, e);
 		}
+		
 		Collections.sort(line);
 		int count = 0;
 		for(int i = 0; i < line.size(); i++) {
 			count += line.get(i).value;
-//			if(line.get(i).value == 1) {
-//				count++;
 			answer = Math.max(answer, count);
-//			}else
-//				count--;
 		}
 		
 		bw.write(String.valueOf(answer));
