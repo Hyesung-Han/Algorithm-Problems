40dca18d2e1e432bc43c25062cfa254e392fd300
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-12-11 00:58:13 +0900

    [Silver I] Title: 신입 사원, Time: 792 ms, Memory: 297880 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -6,45 +6,39 @@ public class Main{
 	static int T;
 	static int N;
 	static int answer;
+	static int [] arr;
 	
 	public static void main(String[] args) throws IOException{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer token = null;
 		
 		T = Integer.parseInt(br.readLine());
 		for(int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
 			
-			answer = 0;
-			int Min = Integer.MAX_VALUE;
-			PriorityQueue<int[]> que = new PriorityQueue<>(new Comparator<int []>() {
-				 
-	            @Override
-	            public int compare(int []o1, int [] o2) {
-	                return o1[0]-o2[0];
-	            }
-	        });
+			StringTokenizer token = null;
+			arr = new int[N];
+			answer = N;
 			
-			for(int n = 0; n < N; n++) {
-				token =  new StringTokenizer(br.readLine());
-				que.offer(new int[] 
-						{Integer.parseInt(token.nextToken()), Integer.parseInt(token.nextToken())});
+			for(int i = 0; i < N; i++) {
+				token = new StringTokenizer(br.readLine());
+				int resume = Integer.parseInt(token.nextToken()) - 1;
+				int interview = Integer.parseInt(token.nextToken()) - 1;
+				
+				arr[resume] = interview;
+			}
+			int m = arr[0];
+			for(int i = 1; i < N; i++) {
+				if(arr[i] > m)
+					answer--;
+				m = Math.min(m, arr[i]);
 			}
 			
-			while(!que.isEmpty()) {
-				int [] q = que.poll();
-				if(q[1] < Min) {
-					Min = q[1];
-					answer++;
-				}
-			}
 			bw.write(String.valueOf(answer)+"\n");
 			
 		}
 		bw.close();
-		
 		
 	}
 
