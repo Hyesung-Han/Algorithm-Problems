0c97ea0a325f3cdc127508f6a0aa208e4bc9d8c7
HYESUNG <87076126+Hyesung-Han@users.noreply.github.com>
2022-12-26 00:32:56 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 76 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main  {
+
+	static int N;
+	static int [] dp;
+	static final int num = 1000000007;
+	
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		dp = new int[N+1];
+		
+		dp[0] = 1;
+		if(N > 0)
+		dp[1] = 1;
+		
+		for(int i = 2; i <= N; i++) {
+			dp[i] = (dp[i-1] + dp[i-2] + 1) % num;
+		}
+		
+		System.out.println(dp[N]);
+	}
+
+}
